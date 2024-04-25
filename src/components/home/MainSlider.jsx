c12028ff9dc8330966233342d546a1e1d0f5ff87
Pear-HyunJI <156041590+Pear-HyunJI@users.noreply.github.com>
2024-04-25 17:41:08 +0900

    메인슬라이드 최종

--- src/components/home/MainSlider.jsx
+++ src/components/home/MainSlider.jsx
@@ -36,252 +36,169 @@ const shaking = keyframes`
 `;
 
 const MainSliderBlock = styled.div`
-margin-top:3650px;
-`
+  position: relative;
+  margin-top: 4100px;
+  width: 100vw;
+  border: 1px solid red;
+
+  .slick-track {
+    margin-top: 410px;
+
+    .slide {
+      height: 50vh;
+      line-height: 50vh;
+      text-align: center;
+      // border: 1px solid blue;
+      img {
+        // border: 1px solid yellow;
+        display: inline-block;
+        width: 160px;
+        //transform: scale(1);
+        //transition: transform 0.5s ease-in-out;
+        animation: ${shaking} 4.5s infinite ease-in-out forwards;
+      }
+    }
+
+    .slick-center {
+      img {
+        width: 240px;
+        // transform: scale(1.5);
+        //transition: transform 0.5s ease-in-out;
+      }
+    }
+  }
+  .text {
+    margin-top: 100px;
+
+    color: #5a4620;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    height: 50vh;
+    span {
+      font-size: 30px;
+      line-height: 70px;
+      letter-spacing: 10px;
+      writing-mode: vertical-lr;
+      left: 50%;
+    }
+  }
+`;
+
 const MainSlider = () => {
-  return <MainSliderBlock>
-    <p>★메인슬라이드★</p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    <p>스크롤 늘리기 <br /></p>
-    </MainSliderBlock>;
+  const options = {
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    centerMode: true,
+    centerPadding: "60px",
+    infinite: true,
+    beforeChange: (next) => {
+      const slideItems = document.querySelectorAll(".slide");
+      slideItems.forEach((item, index) => {
+        if (index === next) {
+          item.classList.add("slick-center");
+        } else {
+          item.classList.remove("slick-center");
+        }
+      });
+    },
+  };
+
+  const sliders = [
+    { src: "./assets/image/slide1/slide_img1.png", alt: "과자1" },
+    { src: "./assets/image/slide1/slide_img2.png", alt: "과자2" },
+    { src: "./assets/image/slide1/slide_img3.png", alt: "과자3" },
+    { src: "./assets/image/slide1/slide_img4.png", alt: "과자4" },
+    { src: "./assets/image/slide1/slide_img5.png", alt: "과자5" },
+    { src: "./assets/image/slide1/slide_img6.png", alt: "과자7" },
+    { src: "./assets/image/slide1/slide_img8.png", alt: "과자8" },
+    { src: "./assets/image/slide1/slide_img9.png", alt: "과자9" },
+    { src: "./assets/image/slide1/slide_img10.png", alt: "과자10" },
+    { src: "./assets/image/slide1/slide_img11.png", alt: "과자11" },
+    { src: "./assets/image/slide1/slide_img12.png", alt: "과자12" },
+  ];
+
+  return (
+    <MainSliderBlock>
+      <div className="text">
+        <span>오늘은。&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <span>과자를 먹는、</span>
+      </div>
+      <Slider className="slider" {...options}>
+        {sliders.map((item, index) => (
+          <div className="slide" key={index}>
+            <img src={item.src} alt={item.alt} />
+          </div>
+        ))}
+      </Slider>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+      <div>늘리기</div>
+    </MainSliderBlock>
+  );
 };
 
 export default MainSlider;
