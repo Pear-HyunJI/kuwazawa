4d58c60c39915fe1105fbf40eb7c0f0ed9677e61
Pear-HyunJI <156041590+Pear-HyunJI@users.noreply.github.com>
2024-05-07 16:28:24 +0900

    Update ProductDetailView.jsx

--- src/views/product/ProductDetailView.jsx
+++ src/views/product/ProductDetailView.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'
-import ProductDetailSection from '@/components/product/ProductDetailSection';
-import ReviewList from '@/components/review/ReviewList';
-
+import React from "react";
+import { useLocation } from "react-router-dom";
+import ProductDetailSection from "@/components/product/ProductDetailSection";
+import ReviewList from "@/components/review/ReviewList";
 
 const ProductDetailView = () => {
-    const location = useLocation()
-    const { item } = location.state
-    
+  const location = useLocation();
+  const { item } = location.state;
 
-    return (
-        <div className='row'>
-            <ProductDetailSection product={item} />
-            <ReviewList />
-        </div>
-    );
+  console.log("아이템", item);
+
+  return (
+    <div className="row">
+      <ProductDetailSection product={item} />
+      <ReviewList product={item} />
+    </div>
+  );
 };
 
 export default ProductDetailView;
